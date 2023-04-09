584ddcf3f9123ecbaebe398af6ddc5845e2b3d96
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2023-04-09 20:38:51 -0300

    first commit

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,8 @@
+import "./CampoTexto.css"
+const CampoTexto = () =>{
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder ="ingresar nombre"/>
+    </div>
+}
+export default CampoTexto
